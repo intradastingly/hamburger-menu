bd982d0d8b7f529fdc8ccbb7cc7eb4069fe62fd0
Asaf Läckgren <asaf_lackgren@hotmail.com>
2020-10-23 18:17:09 +0200

    completed hamburger menu

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 
 function dropDownSlider() {
     let size = document.querySelector('nav');
+    let menuLinks = document.querySelector('.dropDownMenu');
     size.classList.toggle("menuOut");
+    menuLinks.classList.toggle("linksOut")
 }
 
 
